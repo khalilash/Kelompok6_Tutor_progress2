e01d2ca6e05502b87d67d5a990695daa19f75218
cnsalsabila <162676136+cnsalsabila@users.noreply.github.com>
2025-12-08 12:56:14 +0700

    errorrrrrrrrrr

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-
-// HOMEPAGE KAMU
+// HOMEPAGE
 import 'screens/homepage/homepage.dart';
 
-
-// FILE TEMAN (LOKASI BARU)
-import 'screens/login-register/welcome_screen.dart';
+// SCREEN LAIN
 import 'screens/chat/chat_list_screen.dart';
 import 'screens/search/search_page.dart';
 import 'screens/sesi/detail_sesi_page.dart';
@@ -20,16 +18,13 @@ import 'screens/sesi/calling_page.dart';
 import 'screens/sesi/detail_sesi_selesai_page.dart';
 import 'screens/profilepage/profile_page.dart';
 import 'splash_page.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
-final supabase = Supabase.instance.client;
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Supabase.initialize(
     url: 'https://hqcdhreharfszhsdmqlm.supabase.co',
-    anonKey: 
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhxY2RocmVoYXJmc3poc2RtcWxtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ0MDA5NjYsImV4cCI6MjA3OTk3Njk2Nn0.y2yNSnwidqJus5s4XiUTI2KnG0C6W6mXkP8WAUVt9N4',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhxY2RocmVoYXJmc3poc2RtcWxtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ0MDA5NjYsImV4cCI6MjA3OTk3Njk2Nn0.y2yNSnwidqJus5s4XiUTI2KnG0C6W6mXkP8WAUVt9N4',
   );
 
   runApp(const MyApp());
@@ -38,30 +33,26 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
 
-
-      // THEME APP
+      // THEME
       theme: ThemeData(
         textTheme: GoogleFonts.poppinsTextTheme(),
         useMaterial3: false,
       ),
 
-
       // HALAMAN PERTAMA
       home: const SplashPage(),
 
-
-      // ROUTES
+      // ROUTING
       routes: {
         '/homepage': (context) => const HomePage(),
         '/search': (context) => const SearchPage(),
         '/chat': (context) => const ChatListScreen(),
-        '/sesi' : (context) => const DetailSesiPage(),
+        '/sesi': (context) => const DetailSesiPage(),
         '/popupBelumDimulai': (context) => const PopupBelumDimulai(),
         '/downloadSuccess': (context) => const DownloadSuccessPage(),
         '/callingPage': (context) => const CallingPage(),
@@ -69,6 +60,7 @@ class MyApp extends StatelessWidget {
         '/tutorSelesaiPage': (context) => const TutorSelesaiPage(),
         '/videoCallPage': (context) => const VideoCallPage(),
         '/sesiSelesai': (context) => const DetailSesiSelesaiPage(),
+        '/profile': (context) => const ProfilePage(),
       },
     );
   }
